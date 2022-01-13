1cbb563b987449365d639072a8fe78d1bd97bb18
Iris System <iris@iris.ac.nz>
2022-01-14 00:43:37 +1300

    AppFactory: don't print error details in production

--- lib/AppFactory.php
+++ lib/AppFactory.php
@@ -5,7 +5,7 @@ namespace Grip;
 final class AppFactory {
 	public static function create(): \Slim\App {
 		$app = \Slim\Factory\AppFactory::create();
-		$app->addErrorMiddleware(true, false, false);
+		$app->addErrorMiddleware(($_ENV['APP_ENV'] ?? 'production') !== 'production', false, false);
 		$app->add(new \Middlewares\TrailingSlash(false));
 
 		$app->get('/{slug:[^!]*}[!{opts:[a-z,]*}]', \Grip\PageController::class);
